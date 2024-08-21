020de3abae2cb4fa67f9e5fb899865a2257f32f6
HernanR2000 <hernanrodriguez259@gmail.com>
2024-08-20 21:54:32 -0300

    Ejercitacion de clase
    Esto es una ejercitacion de clase

---
+++ Clase 3/script.js
@@ -0,0 +1,22 @@
+
+window.onload=function(){
+
+    var carga = document.getElementById("pantallacarga");
+
+    setTimeout(() => {
+        
+        carga.style.display="none";
+
+    }, 1000);
+
+
+    var carga = document.getElementById("Pantalla-intermedia");
+ 
+    setTimeout(() => {
+        
+        carga.style.display="block";
+
+    }, 1000);
+
+
+}
